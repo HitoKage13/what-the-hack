01f686e4cefa7ce71ec8a5a8716e920932c348e9
haesun <haesunsw@gmail.com>
2020-06-13 02:12:05 +1000

    there u go

--- backend/main.py
+++ backend/main.py
@@ -2,7 +2,16 @@
 Description
 '''
 
-data = {'doctors': [], 'queues': []}
+DATA = {'doctors': [], 'queues': []}
+
+class Patient:
+    def __init__(self, name, age, telephone, emergency, medicare, diseases):
+        self.name = name
+        self.age = name
+        self.telephone = name
+        self.emergency = name
+        self.medicare = name
+        self.diseases = name
 
 def queue_create(name):
     '''
@@ -11,8 +20,35 @@ def queue_create(name):
     Raises:
     '''
     queue = {'name': name, 'patients': []}
-    data['queues'].append(queue)
+    DATA['queues'].append(queue)
 
-def queue():
-    pass
+def queue(name):
+    for queue in DATA['queues']:
+        if queue['name'] == name:
+            return queue
+    raise Exception("Can't find queue")
 
+# def patient_create(name):
+#     new_patient = Patient(name, age, )
+#     for queue in DATA['queues']:
+#         if queue['name'] == name:
+#             return queue
+#     raise Exception("Can't find queue")
+# def help(id, name_queue):
+    
+
+def patient_status(id, status):
+    for q in DATA['queues']:
+        for p in q['patients']:
+            if p['id'] == id:
+                p['status'] = status
+
+# def doctor_create():
+#     new_do
+
+# def help():
+
+#     pass
+
+if __name__ == "__main__":
+    print(DATA)
--- backend/server.py
+++ backend/server.py
@@ -4,168 +4,123 @@ Description
 
 from flask import Flask, request
 
-from werkzeug.exceptions import BadRequest
+# from werkzeug.exceptions import BadRequest
+
+from main import queue_create, queue
 
 from json import dumps
 
 APP = Flask(__name__)
 
 @APP.route('/queue/create', methods=['POST'])
-def queue_create():
+def queue_create_route():
     '''
     A route for
     Params: {}
     Raises: 
     Returns: {}
     '''
-    zid = request.get_json()['zid']
-    description = request.get_json()['description']
-    try:
-        helpr.make_request(zid, description)
-    except (KeyError,  ValueError):
-        raise BadRequest('')
-    return dumps({})
+    print(request.get_json()['name'])
+    # name = request.get_json()['name']
+    # queue_create(name)
 
 @APP.route('/queue', methods=['GET'])
-def queue():
+def queue_route():
     '''
     A route for
     Params: {}
     Raises: 
     Returns: {}
     '''
-    zid = request.get_json()['zid']
-    description = request.get_json()['description']
-    try:
-        helpr.make_request(zid, description)
-    except (KeyError,  ValueError):
-        raise BadRequest('')
-    return dumps({})
+    name = request.get_json()['name']
+    return dumps(queue(name))
 
-@APP.route('/patient/create', methods=['POST'])
-def patient_create():
-    '''
-    A route for
-    Params: {}
-    Raises: 
-    Returns: {}
-    '''
-    zid = request.get_json()['zid']
-    description = request.get_json()['description']
-    try:
-        helpr.make_request(zid, description)
-    except (KeyError,  ValueError):
-        raise BadRequest('')
-    return dumps({})
+# @APP.route('/patient/create', methods=['POST'])
+# def patient_create():
+#     '''
+#     A route for
+#     Params: {}
+#     Raises: 
+#     Returns: {}
+#     '''
+#     zid = request.get_json()['zid']
+#     description = request.get_json()['description']
+#     try:
+#         helpr.make_request(zid, description)
+#     except (KeyError,  ValueError):
+#         raise BadRequest('')
+#     return dumps({})
 
-@APP.route('/patient/move', methods=['POST'])
-def patient_move():
-    '''
-    A route for
-    Params: {}
-    Raises: 
-    Returns: {}
-    '''
-    zid = request.get_json()['zid']
-    description = request.get_json()['description']
-    try:
-        helpr.make_request(zid, description)
-    except (KeyError,  ValueError):
-        raise BadRequest('')
-    return dumps({})
+# @APP.route('/patient/move', methods=['POST'])
+# def patient_move():
+#     '''
+#     A route for
+#     Params: {}
+#     Raises: 
+#     Returns: {}
+#     '''
+#     zid = request.get_json()['zid']
+#     description = request.get_json()['description']
+#     try:
+#         helpr.make_request(zid, description)
+#     except (KeyError,  ValueError):
+#         raise BadRequest('')
+#     return dumps({})
 
-@APP.route('/patient/delete', methods=['DELETE'])
-def patient_delete():
-    '''
-    A route for
-    Params: {}
-    Raises: 
-    Returns: {}
-    '''
-    zid = request.get_json()['zid']
-    description = request.get_json()['description']
-    try:
-        helpr.make_request(zid, description)
-    except (KeyError,  ValueError):
-        raise BadRequest('')
-    return dumps({})
+# @APP.route('/patient/delete', methods=['DELETE'])
+# def patient_delete():
 
-@APP.route('/patient/info', methods=['PUT'])
-def patient_info():
-    '''
-    A route for
-    Params: {}
-    Raises: 
-    Returns: {}
-    '''
-    zid = request.get_json()['zid']
-    description = request.get_json()['description']
-    try:
-        helpr.make_request(zid, description)
-    except (KeyError,  ValueError):
-        raise BadRequest('')
-    return dumps({})
+#     id = request.get_json()['id']
+#     description = request.get_json()['description']
+#     try:
+#         main.make_request(id, description)
+#     except (KeyError,  ValueError):
+#         raise BadRequest('')
+#     return dumps({})
 
-@APP.route('/patient/status', methods=['PUT'])
-def patient_status():
-    '''
-    A route for
-    Params: {}
-    Raises: 
-    Returns: {}
-    '''
-    zid = request.get_json()['zid']
-    description = request.get_json()['description']
-    try:
-        helpr.make_request(zid, description)
-    except (KeyError,  ValueError):
-        raise BadRequest('')
-    return dumps({})
+# @APP.route('/patient/info', methods=['PUT'])
+# def patient_info():
 
-@APP.route('/doctor/create', methods=['POST'])
-def doctor_create():
-    '''
-    A route for
-    Params: {}
-    Raises: 
-    Returns: {}
-    '''
-    zid = request.get_json()['zid']
-    description = request.get_json()['description']
-    try:
-        helpr.make_request(zid, description)
-    except (KeyError,  ValueError):
-        raise BadRequest('')
-    return dumps({})
+#     zid = request.get_json()['id']
+#     description = request.get_json()['description']
+#     try:
+#         main.make_request(zid, description)
+#     except (KeyError,  ValueError):
+#         raise BadRequest('')
+#     return dumps({})
 
-@APP.route('/help', methods=['POST'])
-def help():
-    '''
-    A route for
-    Params: {}
-    Raises: 
-    Returns: {}
-    '''
-    zid = request.get_json()['zid']
-    description = request.get_json()['description']
-    try:
-        helpr.make_request(zid, description)
-    except (KeyError,  ValueError):
-        raise BadRequest('')
-    return dumps({})
+# @APP.route('/patient/status', methods=['PUT'])
+# def patient_status():
 
-@APP.route('/status', methods=['GET'])
-def status():
-    '''
-    A route for
-    Params: {}
-    Raises: 
-    Returns: {}
-    '''
-    zid = request.get_json()['zid']
-    description = request.get_json()['description']
-    try:
-        helpr.make_request(zid, description)
-    except (KeyError,  ValueError):
-        raise BadRequest('')
-    return dumps({})
+#     zid = request.get_json()['id']
+#     description = request.get_json()['description']
+#     try:
+#         main.make_request(zid, description)
+#     except (KeyError,  ValueError):
+#         raise BadRequest('')
+#     return dumps({})
+
+# @APP.route('/doctor/create', methods=['POST'])
+# def doctor_create():
+
+#     id = request.get_json()['id']
+#     description = request.get_json()['description']
+#     try:
+#         main.make_request(id, description)
+#     except (KeyError,  ValueError):
+#         raise BadRequest('')
+#     return dumps({})
+
+# @APP.route('/help', methods=['POST'])
+# def help():
+
+#     id = request.get_json()['id']
+#     description = request.get_json()['description']
+#     try:
+#         main.make_request(id, description)
+#     except (KeyError,  ValueError):
+#         raise BadRequest('')
+#     return dumps({})
+
+if __name__ == "__main__":
+    APP.run(port=5050, debug=True)
