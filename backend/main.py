b65110d9e4c98a4fc22e10de3cb6714bb15530b5
Dasyure <das.yaegar@gmail.com>
2020-06-13 00:07:33 +1000

    creating main.py

---
+++ backend/main.py
@@ -0,0 +1,18 @@
+'''
+Description
+'''
+
+data = {'doctors': [], 'queues': []}
+
+def queue_create(name):
+    '''
+    Description:
+    Params:
+    Raises:
+    '''
+    queue = {'name': name, 'patients': []}
+    data['queues'].append(queue)
+
+def queue():
+    pass
+
