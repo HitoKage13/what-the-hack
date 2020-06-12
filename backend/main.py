80a8470927b9348675cb7231fdc21ded00cc5b91
Dasyure <das.yaegar@gmail.com>
2020-06-13 01:22:09 +1000

    adding main

--- backend/main.py
+++ backend/main.py
@@ -2,7 +2,16 @@
 Description
 '''
 
-data = {'doctors': [], 'queues': []}
+DATA = {'doctors': [], 'queues': []}
+
+class Patient:
+    def __init__(self, name, age, telephone, emergency, medicare, diseases):
+        self.name = name
+        self.age = name
+        self.telephone = name
+        self.emergency = name
+        self.medicare = name
+        self.diseases = name
 
 def queue_create(name):
     '''
@@ -11,8 +20,21 @@ def queue_create(name):
     Raises:
     '''
     queue = {'name': name, 'patients': []}
-    data['queues'].append(queue)
+    DATA['queues'].append(queue)
 
-def queue():
-    pass
+def queue(name):
+    for queue in DATA['queues']:
+        if queue['name'] == name:
+            return queue
+    raise Exception("Can't find queue")
+
+def patient_create(name):
+    new_patient = Patient(name, age, )
+    for queue in DATA['queues']:
+        if queue['name'] == name:
+            return queue
+    raise Exception("Can't find queue")
+
+
+
 
